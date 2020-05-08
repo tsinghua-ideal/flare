1495c0e2cdac5f8ece350896b0ed7a44cde5f2bf
AmbitionXiang <Xiang.Leee@outlook.com>
2020-05-08 20:40:57 +0800

    try to find enclave.signed.so

--- bin/set_env_dist.sh
+++ bin/set_env_dist.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+echo "setting..."
 export VEGA_LOCAL_IP=172.16.111.236
 export VEGA_DEPLOYMENT_MODE=distributed
 export VEGA_LOG_LEVEL=debug
+echo "set."
--- bin/set_env_local.sh
+++ bin/set_env_local.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+echo "setting"
 export VEGA_LOCAL_IP=0.0.0.0
 export VEGA_DEPLOYMENT_MODE=local
 export VEGA_LOG_LEVEL=debug
+echo "set"
