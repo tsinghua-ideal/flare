631e452f9489dd3049c19f9f37f8aa9cb0a73783
AmbitionXiang <Xiang.Leee@outlook.com>
2021-06-30 15:52:24 +0800

    remove dup reduce

--- enclave/src/op/mod.rs
+++ enclave/src/op/mod.rs
@@ -2086,24 +2086,3 @@ pub trait OpE: Op {
     }
 
 }
-
-pub trait Reduce<T> {
-    fn reduce<F>(self, f: F) -> Option<T>
-    where
-        Self: Sized,
-        F: FnMut(T, T) -> T;
-}
-
-impl<T, I> Reduce<T> for I
-where
-    I: Iterator<Item = T>,
-{
-    #[inline]
-    fn reduce<F>(mut self, f: F) -> Option<T>
-    where
-        Self: Sized,
-        F: FnMut(T, T) -> T,
-    {
-        self.next().map(|first| self.fold(first, f))
-    }
-}
