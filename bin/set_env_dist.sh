58722aa4219f029e08d648051cbe8fe2d16089ee
Xiang Li <Xiang.Leee@outlook.com>
2022-09-28 17:59:28 +0800

    fix bugs in distributed mode, sometimes it is blocked.

--- bin/set_env_dist.sh
+++ bin/set_env_dist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "setting..."
-export VEGA_LOCAL_IP=172.16.111.236
+export VEGA_LOCAL_IP=172.20.68.101
 export VEGA_DEPLOYMENT_MODE=distributed
 export VEGA_LOG_LEVEL=debug
 echo "set."
